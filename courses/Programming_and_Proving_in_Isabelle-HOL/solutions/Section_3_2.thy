2a412eb5e7ed707bc5f399ddf7055782a1d85a38
Javier Diaz <javier.diaz@iohk.io>
2018-05-17 23:43:00 -0300

    Clean-up of exercises in Section 3.2.

--- courses/Programming_and_Proving_in_Isabelle-HOL/solutions/Section_3_2.thy
+++ courses/Programming_and_Proving_in_Isabelle-HOL/solutions/Section_3_2.thy
@@ -7,19 +7,19 @@ begin
 datatype 'a tree = Tip | Node "'a tree" 'a "'a tree"
 
 fun set :: "'a tree \<Rightarrow> 'a set" where
-"set Tip = {}" |
+"set Tip          = {}" |
 "set (Node l x r) = {x} \<union> set l \<union> set r"
 
 value "set (Node (Node Tip (2::nat) Tip) 1 (Node Tip 3 (Node Tip 4 Tip)))" (* {2,1,3,4} *)
 
 fun ord :: "int tree \<Rightarrow> bool" where
-"ord Tip = True" |
+"ord Tip          = True" |
 "ord (Node l x r) = (x = Max (set l \<union> {x}) \<and> x = Min (set r \<union> {x}) \<and> ord l \<and> ord r)"
 
 value "ord (Node (Node Tip 0 Tip) 1 (Node Tip 0 Tip))"
 
 fun ins :: "int \<Rightarrow> int tree \<Rightarrow> int tree" where
-"ins x Tip = Node Tip x Tip" |
+"ins x Tip          = Node Tip x Tip" |
 "ins x (Node l y r) = (
   if x < y 
   then Node (ins x l) y r 
@@ -28,29 +28,28 @@ fun ins :: "int \<Rightarrow> int tree \<Rightarrow> int tree" where
     then Node l y (ins x r)
     else Node l y r))"
 
-value "ins 1 (ins 10 (ins 20 (ins 0 (ins 3 (ins 2 (ins 1 Tip))))))"
+value "ins (-1) (ins 1 (ins 10 (ins 20 (ins 0 (ins 3 (ins 2 (ins 1 Tip)))))))"
 
-(*
+(* NOTE: Proving some lemmata, just to be sure about a couple facts. *)
+
 lemma max_01: "x < y \<Longrightarrow> y = Max (set t \<union> {y}) \<Longrightarrow> y = Max (set (ins x t) \<union> {y})"
   apply (induction t rule: tree.induct)
   apply (auto simp add: algebra_simps)
   done
-*)
+
+lemma ins_01 [simp]: "x = y \<Longrightarrow> set (ins x (Node l y r)) = {x} \<union> set (Node l y r) \<and> ord (Node l y r) \<Longrightarrow> ord (ins x (Node l y r))"
+  apply auto
+  done
 
 lemma ins_02 [simp]: "set (ins x t) = {x} \<union> set t"
   apply (induction t rule: tree.induct)
   apply auto
   done
 
+(* TODO: Prove the main theorem. *)
 theorem ins_correctness: "set (ins x t) = {x} \<union> set t \<and> ord t \<Longrightarrow> ord (ins x t)"
   apply (induction t rule: tree.induct)
-  apply auto
+  apply (auto simp add: algebra_simps)
   done
-
-lemma ins_01: "x = y \<Longrightarrow> set (ins x (Node l y r)) = {x} \<union> set (Node l y r) \<and> ord (Node l y r) \<Longrightarrow> ord (ins x (Node l y r))"
-  apply auto
-  done
-
-
 
 end
