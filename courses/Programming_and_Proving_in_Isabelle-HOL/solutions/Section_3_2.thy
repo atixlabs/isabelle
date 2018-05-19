bbb86611e1766388c9a1abebeda99c25f6064109
Javier Diaz <javier.diaz@iohk.io>
2018-05-19 01:04:30 -0300

    Completed exercises in Section 3.2.

--- courses/Programming_and_Proving_in_Isabelle-HOL/solutions/Section_3_2.thy
+++ courses/Programming_and_Proving_in_Isabelle-HOL/solutions/Section_3_2.thy
@@ -12,11 +12,19 @@ fun set :: "'a tree \<Rightarrow> 'a set" where
 
 value "set (Node (Node Tip (2::nat) Tip) 1 (Node Tip 3 (Node Tip 4 Tip)))" (* {2,1,3,4} *)
 
+(* NOTE: "A la" Haskell ;)  *)
+fun all :: "(int \<Rightarrow> bool) \<Rightarrow> int tree \<Rightarrow> bool" where
+"all _ Tip          = True" |
+"all p (Node l x r) = (p x \<and> all p l \<and> all p r)"  
+
 fun ord :: "int tree \<Rightarrow> bool" where
 "ord Tip          = True" |
-"ord (Node l x r) = (x = Max (set l \<union> {x}) \<and> x = Min (set r \<union> {x}) \<and> ord l \<and> ord r)"
+(* NOTE: I will not use Max and Min since I would need to search for lemmas about them. *)
+(* "ord (Node l x r) = (x = Max (set l \<union> {x}) \<and> x = Min (set r \<union> {x}) \<and> ord l \<and> ord r)" *)
+"ord (Node l x r) = ((all (\<lambda>y.(y < x)) l) \<and> (all (\<lambda>y.(y > x)) r) \<and> ord l \<and> ord r)"
 
 value "ord (Node (Node Tip 0 Tip) 1 (Node Tip 0 Tip))"
+value "ord (Node (Node Tip 0 Tip) 1 (Node Tip 2 Tip))"
 
 fun ins :: "int \<Rightarrow> int tree \<Rightarrow> int tree" where
 "ins x Tip          = Node Tip x Tip" |
@@ -30,26 +38,26 @@ fun ins :: "int \<Rightarrow> int tree \<Rightarrow> int tree" where
 
 value "ins (-1) (ins 1 (ins 10 (ins 20 (ins 0 (ins 3 (ins 2 (ins 1 Tip)))))))"
 
-(* NOTE: Proving some lemmata, just to be sure about a couple facts. *)
-
-lemma max_01: "x < y \<Longrightarrow> y = Max (set t \<union> {y}) \<Longrightarrow> y = Max (set (ins x t) \<union> {y})"
-  apply (induction t rule: tree.induct)
-  apply (auto simp add: algebra_simps)
-  done
-
-lemma ins_01 [simp]: "x = y \<Longrightarrow> set (ins x (Node l y r)) = {x} \<union> set (Node l y r) \<and> ord (Node l y r) \<Longrightarrow> ord (ins x (Node l y r))"
-  apply auto
-  done
-
-lemma ins_02 [simp]: "set (ins x t) = {x} \<union> set t"
+(* Auxiliary lemma: If y is the greatest element in t and x < y then inserting x in t doesn't change that. *)
+lemma all_01 [simp]: "x < y \<Longrightarrow> all (\<lambda>z.(z < y)) t \<Longrightarrow> all (\<lambda>z.(z < y)) (ins x t)"
   apply (induction t rule: tree.induct)
   apply auto
   done
 
-(* TODO: Prove the main theorem. *)
+(* Auxiliary lemma: If y is the least element in t and x > y then inserting x in t doesn't change that. *)
+lemma all_02 [simp]: "x > y \<Longrightarrow> all (\<lambda>z.(z > y)) t \<Longrightarrow> all (\<lambda>z.(z > y)) (ins x t)"
+  apply (induction t rule: tree.induct)
+  apply auto
+  done
+
+(* Auxiliary lemma: ins preserves order. *)
+lemma ins_preserves_order [simp]: "ord t \<Longrightarrow> ord (ins x t)"
+  apply (induction t rule: ins.induct)
+  apply auto
+  done
+
 theorem ins_correctness: "set (ins x t) = {x} \<union> set t \<and> ord t \<Longrightarrow> ord (ins x t)"
-  apply (induction t rule: tree.induct)
-  apply (auto simp add: algebra_simps)
+  apply auto
   done
 
 end
