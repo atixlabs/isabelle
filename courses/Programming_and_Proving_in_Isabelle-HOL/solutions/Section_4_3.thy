ee5408834832cda54d1e69c16eda8baa09cb2bf2
Javier Diaz <javier.diaz@iohk.io>
2018-05-28 13:57:00 -0300

    Completed Exercise 4.1.

--- courses/Programming_and_Proving_in_Isabelle-HOL/solutions/Section_4_3.thy
+++ courses/Programming_and_Proving_in_Isabelle-HOL/solutions/Section_4_3.thy
@@ -2,91 +2,25 @@ theory Section_4_3
 imports Main
 begin
 
-lemma "(x::nat) = 0 \<longleftrightarrow> x + 1 = 1"
-proof
-  assume "x = 0"
-  thus "x + 1 = 1" by simp
-next
-  assume "x + 1 = 1"
-  thus "x = 0" by simp
-qed
+(* Exercise 4.1 *)
 
-lemma "0 = 0 \<or> x = y"
+lemma 
+  assumes 
+    T: "\<forall>x y. T x y \<or> T y x"
+  and 
+    A: "\<forall>x y. A x y \<and> A y x \<longrightarrow> x = y"
+  and 
+    TA: "\<forall>x y. T x y \<longrightarrow> A x y" 
+  and 
+    "A x y"
+  shows "T x y"
 proof cases
-  assume "x = y"
-  hence "0 = 0 \<or> True" by simp
-  thus "0 = 0 \<or> x = y" by simp
+  assume "T x y"
+  thus ?thesis by simp
 next
-  assume "x \<noteq> y"
-  hence "0 = 0 \<or> False" by simp
-  thus "0 = 0 \<or> x = y" by simp
+  assume "\<not> T x y"
+  hence "T y x" using T by blast 
+  hence "A y x" using TA by simp
+  hence "x = y" using `A x y` and A by simp
+  thus ?thesis using `T y x` by simp  
 qed
-
-lemma "\<not>(x \<and> \<not> x)"
-proof
-  assume 0: "x \<and> \<not> x"
-  from 0 have 1:  "x \<longrightarrow> x \<and> \<not>x" by simp
-  from 0 have 2: "\<not>x \<longrightarrow> \<not>x \<and> \<not>\<not>x" by simp
-  from 1 and 2 have "(x \<longrightarrow> False) \<and> (\<not>x \<longrightarrow> False)" by simp
-  thus "False" by simp
-qed
-
-lemma "\<forall>x. (x::nat) + 1 > x"
-proof
-  fix x
-  show "(x::nat) + 1 > x" by simp
-qed
-
-lemma "\<exists>n. (n::nat) + 1 = 1"
-proof - 
-  have "\<exists>n. (n::nat) + 1 = 1" by simp
-  then obtain n where p: "(n::nat) + 1 = 1" by simp
-  then show ?thesis by (rule exI) 
-  (* show "(0::nat) + 1 = 1" by simp *) 
-qed
-
-lemma "A \<union> {} = A"
-proof
-  show "A \<union> {} \<subseteq> A"
-  proof
-    fix x
-    assume "x \<in> A \<union> {}"
-    hence "x \<in> A \<or> x \<in> {}" by simp
-    thus "x \<in> A" by simp
-  qed
-next
-  show "A \<subseteq> A \<union> {}"
-  proof
-    fix x
-    assume "x \<in> A"
-    hence "x \<in> A \<or> x \<in> {}" by simp
-    thus "x \<in> A \<union> {}" by simp
-  qed
-qed
-
-assume "surj f"
-from this have "\<exists> a. {x . x \<notin> f x } = f a" by (auto simp: surj_def )
-from this show "False" by blast
-qed
-
-proof
-assume "surj f"
-hence "\<exists> a. {x . x \<noteq> f x } = f a" by(auto simp: surj_def )
-thus "False" by blast
-qed
-
-
-
-
-
-
-lemma
-fixes f :: "'a \<Rightarrow> 'a set"
-assumes s: "surj f"
-shows "False"
-proof - 
-  have "\<exists> a. {x . x \<notin> f x} = f a" using s by (auto simp: surj_def)
-	thus "False" by blast
-qed
-
-end
