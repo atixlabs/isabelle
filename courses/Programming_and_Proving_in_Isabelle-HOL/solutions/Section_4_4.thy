e6b499733eb646f8404f031e2b8cc2586d50d34d
Javier Diaz <javier.diaz@iohk.io>
2018-05-29 18:52:21 -0300

    Completed Exercise 4.3.

---
+++ courses/Programming_and_Proving_in_Isabelle-HOL/solutions/Section_4_4.thy
@@ -0,0 +1,35 @@
+theory Section_4_4
+imports Main
+begin
+
+(* Exercise 4.3 *)
+
+inductive ev :: "nat \<Rightarrow> bool" where
+ev0: "ev 0" |
+evSS : "ev n \<Longrightarrow> ev (n + 2)"
+
+(* NOTE: For some reason the technique explained in Section 4.4.7 didn't work. The following code 
+   hangs:
+
+    lemma assumes a: "ev (Suc(Suc n))" shows "ev n"
+    proof (induction "Suc (Suc m)" arbitrary: m rule: ev.induct)
+    ...
+
+  Therefore, I decided to use the other technique that reformulates the goal, that is, rewrites 
+    ev (Suc(Suc n)) \<Longrightarrow> ev n
+  as
+    ev m \<Longrightarrow> m = Suc(Suc n) \<Longrightarrow> ev n
+ *)
+lemma 
+  assumes "ev m" 
+  shows "m = Suc(Suc n) \<Longrightarrow> ev n"
+  using assms
+proof (induction m arbitrary: n rule: ev.induct)
+   case ev0
+   then show ?case by (simp add: ev.ev0)
+next
+   case evSS
+   then show ?case by (simp add: ev.evSS)
+qed
+
+end
